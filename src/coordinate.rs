24991d0b740d48bcbe6d193df78cb5cdba4f677d
cmarcbs7 <cmarcbs7@gmail.com>
2019-07-07 12:56:18 -0500

    Added docs

--- src/coordinate.rs
+++ src/coordinate.rs
@@ -129,8 +129,8 @@ impl From<(i32, i32, i32)> for Cube {
 ///
 /// let axial = Axial::from_coords(1, 2);
 ///
-/// assert_eq!(Cube::from(axial), Cube::from_coords(1, -3, 2));
 /// assert_eq!(Cube::from(Axial::ORIGIN), Cube::ORIGIN);
+/// assert_eq!(Cube::from(axial), Cube::from_coords(1, -3, 2));
 /// ```
 impl From<Axial> for Cube {
     fn from(coord: Axial) -> Self {
@@ -527,18 +527,45 @@ impl Cube {
 // Axial Coordinate System
 //////////////////////////////////////////////////////////////////////////////
 
+/// Axial coordinates use the same system as cube coordinates, but only store
+/// two of the coordinate values. This is possible since, for cube coordinate
+/// (x, y, z), the third value can always be calculated when the other two are
+/// known due to the constraint x + y + z == 0.
 #[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Axial {
-    q: i32,
-    r: i32,
+    pub q: i32,
+    pub r: i32,
 }
 
+/// For two-element tuple of unsigned 32-bit integers (a, b), the
+/// corresponding axial coordinate is (q, r), where q == a and r == b.
+///
+/// # Examples
+///
+/// ```
+/// use chickenwire::coordinate::Axial;
+///
+/// assert_eq!(Axial::from((1, 2)), Axial { q: 1, r: 2 });
+/// ```
 impl From<(i32, i32)> for Axial {
     fn from((q, r): (i32, i32)) -> Self {
         Self { q: q, r: r }
     }
 }
 
+/// For cube coordinate (x, y, z), the corresponding axial coordinate is
+/// (q, r), where q == x and r == z.
+///
+/// # Examples
+///
+/// ```
+/// use chickenwire::coordinate::{Cube, Axial};
+///
+/// let cube = Cube::from_coords(1, 2, -3);
+///
+/// assert_eq!(Axial::from(Cube::ORIGIN), Axial::ORIGIN);
+/// assert_eq!(Axial::from(cube), Axial::from_coords(1, -3));
+/// ```
 impl From<Cube> for Axial {
     fn from(coord: Cube) -> Self {
         Self { q: coord.x, r: coord.z }
@@ -550,6 +577,7 @@ impl Axial {
     // Constants
     //////////////////////////////////
 
+    /// Axial coordinate origin of (0, 0).
     pub const ORIGIN: Axial = Axial { q: 0, r: 0 };
 
     //////////////////////////////////
@@ -574,6 +602,22 @@ impl Axial {
     // Distances
     //////////////////////////////////
 
+    /// Determines the distance between two axial coordinates.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use chickenwire::coordinate::Axial;
+    ///
+    /// let origin = Axial::ORIGIN;
+    /// let coord_1 = Axial { q: 1, r: -3 };
+    /// let coord_2 = Axial { q: -8, r: 2 };
+    ///
+    /// assert_eq!(origin.dist(coord_1), 3);
+    /// assert_eq!(coord_1.dist(origin), 3);
+    /// assert_eq!(coord_1.dist(coord_1), 0);
+    /// assert_eq!(coord_2.dist(coord_1), 9);
+    /// ```
     pub fn dist(self, other: Self) -> i32 {
         Cube::from(self).dist(Cube::from(other))
     }
