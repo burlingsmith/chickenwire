0623bf0c590bfd984b5abc212b49e270c08345fa
cmarcbs7 <cmarcbs7@gmail.com>
2019-07-14 13:06:59 -0500

    Implemented Default for HexGrid

--- src/geometry.rs
+++ src/geometry.rs
@@ -2,23 +2,11 @@
 
 use crate::hexgrid::*;
 
-pub type Position = (f64, f64);
+//////////////////////////////////////////////////////////////////////////////
+// Single Hexes
+//////////////////////////////////////////////////////////////////////////////
 
-#[derive(Debug)]
-pub enum Compass {
-    North,       // direction   0 degrees clockwise to center, before rotation
-    NortheastA,  // direction  30 degrees clockwise to center, before rotation
-    NortheastB,  // direction  60 degrees clockwise to center, before rotation
-    East,        // direction  90 degrees clockwise to center, before rotation
-    SoutheastA,  // direction 120 degrees clockwise to center, before rotation
-    SoutheastB,  // direction 150 degrees clockwise to center, before rotation
-    South,       // direction 180 degrees clockwise to center, before rotation
-    SouthwestA,  // direction 210 degrees clockwise to center, before rotation
-    SouthwestB,  // direction 240 degrees clockwise to center, before rotation
-    West,        // direction 270 degrees clockwise to center, before rotation
-    NorthwestA,  // direction 300 degrees clockwise to center, before rotation
-    NortheastB,  // direction 330 degrees clockwise to center, before rotation
-}
+pub type Position = (f64, f64);
 
 #[derive(Debug)]
 pub struct Hexagon {
@@ -28,23 +16,30 @@ pub struct Hexagon {
 }
 
 impl Hexagon {
-    pub fn width(&self) -> f64 {
+    pub fn reg_width(&self) -> f64 {
         sqrt(3) * self.side_len
     }
 
-    pub fn height(&self) -> f64 {
+    pub fn reg_height(&self) -> f64 {
         2 * self.side_len
     }
 
-    pub fn hspacing(&self) -> f64 {
+    pub fn reg_horizontal_spacing(&self) -> f64 {
         self.width()
     }
 
-    pub fn vspacing(&self) -> f64 {
+    pub fn reg_vertical_spacing(&self) -> f64 {
         1.5 * self.side_len
     }
 
-    pub fn corners(&self) -> Vec<Position> {
+    pub fn reg_corners(&self) -> Vec<Position> {
         unimplemented!();
     }
 }
+
+//////////////////////////////////////////////////////////////////////////////
+// Hexagonal Grids
+//////////////////////////////////////////////////////////////////////////////
+
+#[derive(Debug)]
+pub struct HexNet {}
--- src/hexgrid.rs
+++ src/hexgrid.rs
@@ -1,4 +1,9 @@
 //! Hexagonal grid
+//!
+//! # Modifying HexGrid Layouts
+//! Just change the `tilt`, `parity`, and/or `sys` field(s) in the `HexGrid`
+//! instance. These parameters aren't central to the actual representation of
+//! the grid, so changing them is cheap and painless.
 
 use petgraph::graph::NodeIndex;
 use petgraph::stable_graph::StableGraph;
@@ -21,7 +26,23 @@ use crate::coordinate::*;
 //
 //////////////////////////////////////////////////////////////////////////////
 
-type HexGraph<T> = StableGraph<T, (), Undirected>;
+#[derive(Debug)]
+pub enum Compass {
+    North,
+    Northeast,
+    East,
+    Southeast,
+    South,
+    Southwest,
+    West,
+    Northwest,
+}
+
+impl Compass {
+    // add code here
+}
+
+type HexGraph<T> = StableGraph<T, Compass>;
 
 type HexMap = HashMap<Cube, NodeIndex>;
 
@@ -54,12 +75,26 @@ struct HexGrid<T> {
     map: HexMap,
 }
 
+impl<T> Default for HexGrid<T> {
+    fn default() -> Self {
+        Self {
+            tilt: Tilt::Flat,
+            parity: Parity::Odd,
+            sys: CoordSys::Cube,
+            graph: StableGraph::new(),
+            map: HashMap::new(),
+        }
+    }
+}
+
 impl<T> HexGrid<T> {
     //////////////////////////////////
     // Utilities
     //////////////////////////////////
 
-    fn cube_coord(&self, coord: MultiCoord) -> Cube {
+    /// Convert a `MultiCoord` to its `Cube` equivalent for the given
+    /// `HexGrid`.
+    fn cube_from(&self, coord: MultiCoord) -> Cube {
         match CoordSys::from(coord) {
             Offset => {
                 let offset = Offset::from(coord);
@@ -83,21 +118,67 @@ impl<T> HexGrid<T> {
         }
     }
 
+    /// Return the `NodeIndex` of the node at the given coordinate if it
+    /// exists.
     fn graph_index(&self, coord: MultiCoord) -> Option<&NodeIndex> {
-        self.map.get(&self.cube_coord(coord))
+        self.map.get(&self.cube_from(coord))
+    }
+
+    ///
+    fn single_insert(&mut self, coord: MultiCoord, data: T) {
+        match self.graph_index(coord) {
+            Some(index) => {},
+            _ => {},
+        }
+    }
+
+    fn single_link_burst(&mut self, coord: MultiCoord) {
+        match self.graph_index(coord) {
+            Some(index) => {
+                let ncoords = self.cube_from(coord).neighbors();
+
+                for neighbor in &ncoords {
+
+                }
+            }
+            _ => (),
+        }
     }
 
     //////////////////////////////////
     // Initialization
     //////////////////////////////////
 
-    pub fn new() -> Self {
-        unimplemented!();
+    /// Creates an empty `HexGrid` with the given parameters.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// unimplemented!();
+    /// ```
+    pub fn new(tilt: Tilt, parity: Parity, sys: CoordSys) -> Self {
+        Self {
+            tilt: tilt,
+            parity: parity,
+            sys: sys,
+            ..Default::default()
+        }
     }
 
-    pub fn new_radial(radius: u32, tilt: Tilt) -> Self {
+    /// Creates a new, radial `HexGrid` with a given number of hexes from
+    /// center to edge. `radius` includes the center hex, so a `radius` of 0
+    /// would result in an empty `HexGrid`.
+    ///
+    /// `sys` defaults to `CoordSys::Cube` in the instantiated `HexGrid`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// unimplemented!();
+    /// ```
+    pub fn new_radial(radius: u32) -> Self {
         if radius == 0 {
-            HexGrid::new()
+            Default::default()
         } else {
             let new_hexes = Cube::ORIGIN.spiral(radius);
 
@@ -110,7 +191,19 @@ impl<T> HexGrid<T> {
         }
     }
 
-    pub fn new_boxy(rows: u32, cols: u32, tilt: Tilt, parity: Parity) -> Self {
+    /// Creates a new, rectangular `HexGrid` with a given number of rows and
+    /// columns.
+    ///
+    /// Initial values of `tilt`, `parity`, and `sys`` for `HexGrid`s
+    /// instantiated with this method are `Tilt::Flat`, `Parity::Odd`, and
+    /// `CoordSys::Offset`, respectively.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// unimplemented!();
+    /// ```
+    pub fn new_boxy(rows: u32, cols: u32) -> Self {
         for row in 0..rows {
             for col in 0..cols {
                 unimplemented!();
@@ -121,9 +214,46 @@ impl<T> HexGrid<T> {
     }
 
     //////////////////////////////////
-    //
+    // Boolean Analysis
     //////////////////////////////////
 
+    /// Returns `true` if the calling instance has a value associated with the
+    /// given coordinate, else `false`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// unimplemented!();
+    /// ```
+    pub fn contains_coord(&self, coord: MultiCoord) -> bool {
+        self.graph_index(coord).is_some()
+    }
+
+    /// Returns `true` if the calling instance contains one or more hex values
+    /// equal to the given target, else `false`. The target type must have the
+    /// `Eq` trait.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// unimplemented!();
+    /// ```
+    pub fn contains_value(&self, target: T) -> bool {
+        unimplemented!();
+    }
+
+    //////////////////////////////////
+    // Retrieval
+    //////////////////////////////////
+
+    /// Return an immutable reference to the data contained at the given
+    /// coordinate within the grid, if it exists.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// unimplemented!();
+    /// ```
     pub fn get(&self, coord: MultiCoord) -> Option<&T> {
         match self.graph_index(coord) {
             Some(&index) => self.graph.node_weight(index),
@@ -131,11 +261,113 @@ impl<T> HexGrid<T> {
         }
     }
 
+    /// Return a mutable reference to the data contained at the given
+    /// coordinate within the grid, if it exists.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// unimplemented!();
+    /// ```
     pub fn get_mut(&mut self, coord: MultiCoord) -> Option<&mut T> {
         match self.graph_index(coord) {
             Some(&index) => self.graph.node_weight_mut(index),
             _ => None,
         }
+    }
+
+    //////////////////////////////////
+    // Extension & Modification
+    //////////////////////////////////
+
+    /// Strictly add a hex value to the grid at a given coordinate. Returns a
+    /// `Result::Err(String)` if there is already a value at the given
+    /// coordinate. Otherwise, returns `Result::Ok(())`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use chickenwire::hexgrid::HexGrid;
+    /// use chickenwire::coordinate::{Cube, MultiCoord};
+    ///
+    /// let &mut grid = HexGrid::new();
+    ///
+    /// let coord_1 = MultiCoord::from(Cube::from_coords(1, 2, -3));
+    /// let coord_2 = MultiCoord::from(Cube::from_coords(3, -4, 1));
+    ///
+    /// unimplemented!();
+    /// ```
+    pub fn add(&mut self, coord: MultiCoord) -> Result<(), String> {
+        if self.contains_coord(coord) {
+            let str = format!("Grid already contains a value at {:?}", coord);
+
+            Result::Err(str)
+        } else {
+            unimplemented!();
+
+            Result::Ok(())
+        }
+    }
+
+    /// Strictly update a pre-existing hex value at a given coordinate.
+    /// Returns a `Result::Err(String)` if the position is vacant. Otherwise,
+    /// returns `Result::Ok(())`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use chickenwire::hexgrid::HexGrid;
+    /// use chickenwire::coordinate::{Cube, MultiCoord};
+    ///
+    /// let &mut grid = HexGrid::new();
+    ///
+    /// let coord_1 = MultiCoord::from(Cube::from_coords(1, 2, -3));
+    /// let coord_2 = MultiCoord::from(Cube::from_coords(3, -4, 1));
+    ///
+    /// unimplemented!();
+    /// ```
+    pub fn update(&mut self, coord: MultiCoord) -> Result<(), String> {
+        if self.contains_coord(coord) {
+            unimplemented!();
+
+            Result::Ok(())
+        } else {
+            let str = format!("Grid contains no value at {:?}", coord);
+
+            Result::Err(str)
+        }
+    }
+
+    /// Either creates & adds or updates a hex value at the given coordinate.
+    ///
+    /// # Examples
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use chickenwire::hexgrid::HexGrid;
+    /// use chickenwire::coordinate::{Cube, MultiCoord};
+    ///
+    /// let &mut grid = HexGrid::new();
+    ///
+    /// let coord_1 = MultiCoord::from(Cube::from_coords(1, 2, -3));
+    /// let coord_2 = MultiCoord::from(Cube::from_coords(3, -4, 1));
+    ///
+    /// unimplemented!();
+    /// ```
+    pub fn set() {
+        unimplemented!();
+    }
+
+    /// Cleanly removes a hex and its associated data from the grid.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// unimplemented!();
+    /// ```
+    pub fn remove(&mut self, coord: MultiCoord) {
+        unimplemented!();
     }
 }
 
